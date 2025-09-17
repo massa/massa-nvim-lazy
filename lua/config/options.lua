0ad9de41e022d5d4fb241bb93e37dc23311070db
Humberto Massa <humbertomassa@gmail.com>
2025-09-17 07:05:13 -0300

    backup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.guifont = "VictorMono Nerd Font Mono:h10"
+vim.opt.backup = true
 vim.o.winborder = "rounded"
 vim.o.shell = "/bin/fish"
