4a15aff37dd4dbd443276a6c7c9db675a2932497
Humberto Massa <humbertomassa@gmail.com>
2026-04-20 08:39:58 -0300

    backups

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.guifont = "VictorMono Nerd Font Mono:h10"
+-- Set backup directory
+local backup_dir = vim.fn.stdpath("data") .. "/backup"
+vim.opt.backupdir = { backup_dir .. "//" } -- // stores files with full path
 vim.opt.backup = true
+vim.opt.writebackup = true
+
+-- Ensure the directory exists
+if vim.fn.isdirectory(backup_dir) == 0 then
+  vim.fn.mkdir(backup_dir, "p")
+end
+vim.opt.guifont = "VictorMono Nerd Font Mono:h10"
 vim.o.winborder = "rounded"
 vim.o.shell = "/bin/fish"
