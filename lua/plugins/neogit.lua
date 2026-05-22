77cd535d1f0b2490d7a547ff37201041de3f7275
Humberto Massa <humbertomassa@gmail.com>
2026-05-22 11:28:43 -0300

    feat: ✨ neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -2,9 +2,15 @@ return {
   "NeogitOrg/neogit",
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
-    "sindrets/diffview.nvim", -- optional - Diff integration
+    -- "sindrets/diffview.nvim", -- optional - Diff integration
+    "esmuellert/codediff.nvim", -- optional
 
     -- Only one of these is needed.
-    "ibhagwan/fzf-lua", -- optional
+    -- "ibhagwan/fzf-lua", -- optional
+    "folke/snacks.nvim", -- optional
+  },
+  cmd = "Neogit",
+  keys = {
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
   },
 }
